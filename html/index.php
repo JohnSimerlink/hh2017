74ec80a097e28b9a202e0b11b114a2e4fc033a48
johnsimer <johnsimerlink@gmail.com>
2016-02-19 02:42:22 -0500

    stuff

--- html/index.php
+++ html/index.php
@@ -7,36 +7,25 @@
 
 </script>
 
-    <body
-	<div
-		<div class="input-field col s12 green-text">
-                    <input id="password" name = "password" type="password" class="validate">
-                    <label for="first_name">Password</label>
-                  </div>
-                  <button class="btn waves-effect waves-light green" type="submit" name="action">Submit
-                    <i class="mdi-content-send right"></i>
-                  </button>>
-		<input type='text' name ='username' id='username'><br>
-		<input type='password' name='password' id='password'><br>
-		<input hidden type='text' name='p' id='p'>
-	</div>
-	<div id='container'>
-	<canvas id="c"></canvas>
-    	     <div id='message' data-message=0>	
-		<?php echo determineWhichQuestionToGoTo($questions); ?>
-   	     </div>
-	</div>
-	<style>
-	#message{
-    position : absolute;    
-    width    : 700px;
-    height   : 200px;
-    left     : 50%;
-    top      : 50%;
-    margin-left : -100px; /* half of the width  */
-    margin-top  : -100px; /* half of the height */
-	}
-	</style>
+    <body>
+      <? require_once 'loginMenu.php' ?>
+    	<div id='container'>
+    	<canvas id="c"></canvas>
+        	     <div id='message' data-message=0>	
+    		<?php echo determineWhichQuestionToGoTo($questions); ?>
+       	     </div>
+    	</div>
+    	<style>
+  	#message{
+      position : absolute;    
+      width    : 700px;
+      height   : 200px;
+      left     : 50%;
+      top      : 50%;
+      margin-left : -100px; /* half of the width  */
+      margin-top  : -100px; /* half of the height */
+  	}
+  	</style>
 <?php
 
 require_once 'footer.php';?> 
