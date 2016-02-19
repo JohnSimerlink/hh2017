7880a47dc2b83d32b6678b67f4e82afc5e4edf73
johnsimer <johnsimerlink@gmail.com>
2016-02-19 07:12:04 -0500

    removed sesion tracking

--- html/answers.php
+++ html/answers.php
@@ -32,7 +32,7 @@ switch ($questionNumber) {
 		break;
 }
 
-echo "quesion number is $questionNumber";
+echo "quesion number is $questionNumber. answer = " . $answers[0];
 function answer4($username, $password, $dbh){
 	$userId = 1;
 	$sql = "SELECT password, is_admin FROM users WHERE username = '$username'";//still allow a certain level of sql injection
